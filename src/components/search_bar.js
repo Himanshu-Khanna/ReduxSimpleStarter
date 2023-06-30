6a40d4fce60631fde81c53288f7a49473ffd42e3
Himanshu-Khanna <himanshu.khanna@terralogic.com>
2023-06-30 15:10:40 +0530

    setup and starting yt api serach

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SerachBar = () => {
+    return <input />; // JSX is just syntactic sugar for JavaScript
+};
+
+export default SerachBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SerachBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY ='AIzaSyD77w4st73HLdVLPyXyM8OqYmz8xpPNGmw';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Creeate a new commponent. This component should produce a HTML
+const App = () => {
+    return (
+    <div>
+        <SerachBar />
+    </div>
+    ); // JSX is just syntactic sugar for JavaScript
+}
+
+// Take this component generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
