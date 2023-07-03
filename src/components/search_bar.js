0dbc19989db37b0cb8fd46412aaff49f2fe169e9
Himanshu-Khanna <himanshu.khanna@terralogic.com>
2023-07-03 11:21:45 +0530

    Update search_bar.js

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SerachBar = () => {
-    return <input />; // JSX is just syntactic sugar for JavaScript
-};
+class SerachBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term:'' };
+    }
+    render() {
+        return (
+            <div className="w-50 flex">
+                <input onChange={ e => this.setState({term: e.target.value})} />
+            </div>
+        );
+    }
+}
 
 export default SerachBar;
